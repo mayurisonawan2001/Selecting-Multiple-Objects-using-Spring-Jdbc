ef676ffcd1cbccfcfa062c21bd3ff125aa72a200
mayurisonawan2001 <75942262+mayurisonawan2001@users.noreply.github.com>
2025-06-17 16:00:23 +0530

    Create StudentDaoImpl.java

---
+++ StudentDaoImpl.java
@@ -0,0 +1,29 @@
+package com.springjdbc.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import com.springjdbc.Student;
+import java.util.List;
+
+public class StudentDaoImpl implements StudentDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Student getStudentById(int studentId) {
+        String query = "SELECT * FROM Student WHERE id = ?";
+        RowMapper<Student> rowMapper = new StudentRowMapper();
+        return jdbcTemplate.queryForObject(query, rowMapper, studentId);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        String query = "SELECT * FROM Student";
+        RowMapper<Student> rowMapper = new StudentRowMapper();
+        return jdbcTemplate.query(query, rowMapper);
+    }
+}
